abb8320725aa892dd6d2fdb8798e03ef43460f6b
mainlycricket <metusharjain@gmail.com>
2023-07-02 20:39:23 +0530

    Student Experience Fetch

--- public/student/js/student.js
+++ public/student/js/student.js
@@ -1,72 +1,81 @@
-window.onload = function() {
-    checkAlreadyLogged();
-  };
-  
-  function checkAlreadyLogged(){
-    fetch("/api/v1/user/whoami/")
-    .then(response => response.json())    
+window.onload = async function () {
+  checkAlreadyLogged();
+  await fetchExperience();
+};
+
+function checkAlreadyLogged() {
+  fetch("/api/v1/user/whoami/")
+    .then((response) => response.json())
     .then((data) => {
-      
-      if(data?.user?.role==='admin'){
-          window.location.href="/admin/index.html"
-      }else if(data?.user?.role!=='student'){
-         window.location.href="/"
+      if (data?.user?.role === "admin") {
+        window.location.href = "/admin/index.html";
+      } else if (data?.user?.role !== "student") {
+        window.location.href = "/";
       }
       // console.log(JSON.stringify(data))
-    })  
-      .catch((error) => {
-        // console.log('error', error)
-      });
+    })
+    .catch((error) => {
+      // console.log('error', error)
+    });
+}
+
+// STUDENT EXPERIENCE FETCH
+async function fetchExperience() {
+  try {
+    const response = await fetch("/api/v1/student/experience");
+    const data = await response.json();
+    console.log(data);
+
+    const { experiences } = data;
+
+    const experienceParent = document.getElementById("experience-container");
+    for (let experience of experiences) {
+      let { jobProfile, company, startDate, endDate, _id } = experience;
+
+      startDate = startDate.split("T")[0];
+
+      let html = `
+      <div class="exp-info-text">
+        <div class="prof-exp-item" id=${_id}>
+          <h2>${company}</h2>
+          <div class="exp-inner-wrap">
+            <div><h3>Job Role:</h3><span>${jobProfile}</span></div>  
+          </div>    
+          <div class="exp-inner-wrap">
+            <div><h3>Start Date:</h3><span>${startDate}</span></div>`;
+
+      if (endDate) {
+        endDate = endDate.split("T")[0];
+        html += ` <div><h3>End Date:</h3><span>${endDate}</span></div>`;
+      }
+
+      html += `</div></div></div>`;
+      experienceParent.innerHTML += html;
+    }
+  } catch (error) {
+    console.log("failed to fetch error", error);
   }
+}
 
-
-
-  // STUDENT EXPERIENCE FETCH
-  function fetchExperience(){
-    var myHeaders = new Headers();
-    var requestOptions = {
-      method: 'GET',
-      headers: myHeaders,
-      redirect: 'follow'
-    };
-
-    fetch("http://localhost:5000/api/v1/student/experience", requestOptions)
-      .then(response => response.json())
-      .then((result) => {
-      // ADD HERE CODE FOR HTML
-  
-        console.log(result)
-      })
-      .catch(error => console.log('error', error));
-  }fetchExperience();
-
-
-
-
-  
-
-
-
-
-const tabButtons = document.querySelectorAll('.prof-tab-links');
-for(let i=0;i<tabButtons.length;i++){
-  tabButtons[i].addEventListener('click',function(e){
+const tabButtons = document.querySelectorAll(".prof-tab-links");
+for (let i = 0; i < tabButtons.length; i++) {
+  tabButtons[i].addEventListener("click", function (e) {
     e.preventDefault();
     let tabName = this.dataset.tab;
-    let tabContent=document.getElementById(tabName);
+    let tabContent = document.getElementById(tabName);
 
     let allTabContent = document.querySelectorAll(".prof-tab-content");
     let allTabButtons = document.querySelectorAll(".prof-tab-links");
 
-    for(let j=0;j<allTabContent.length;j++){
-      allTabContent[j].style.display="none";
+    for (let j = 0; j < allTabContent.length; j++) {
+      allTabContent[j].style.display = "none";
     }
-    for(let k=0;k<allTabButtons.length;k++){
-      allTabButtons[k].classList.remove('active');
+    for (let k = 0; k < allTabButtons.length; k++) {
+      allTabButtons[k].classList.remove("active");
     }
-    tabContent.style.display="block";
+    tabContent.style.display = "block";
     this.classList.add("active");
   });
 }
 
-document.querySelector('.prof-tab-links').click();
+document.querySelector(".prof-tab-links").click();
